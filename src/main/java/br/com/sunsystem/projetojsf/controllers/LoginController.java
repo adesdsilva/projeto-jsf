199480f9df7d27b85e0fed55659655f69ee63282
adesdsilva <adesdsilva@gmail.com>
2021-06-23 00:01:12 -0300

    Nome do projeto: projeto-jsf versão 1.1 Descrição: Tela de login e redirecionamento p/ página home.

---
+++ src/main/java/br/com/sunsystem/projetojsf/controllers/LoginController.java
@@ -0,0 +1,54 @@
+package br.com.sunsystem.projetojsf.controllers;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "loginControl")
+public class LoginController {
+
+	private String usuario;
+	
+	private String password;
+	
+	public void logar() {
+		if (usuario.equals("admin") && password.equals("12345")) {
+			try {
+				this.redirecionar("home.xhtml");
+			} catch (IOException e) {
+				FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario", 
+						new FacesMessage(FacesMessage.SEVERITY_FATAL, "Página não encontrada!", ""));
+				e.printStackTrace();
+			}
+		} else {
+			FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario", 
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário/Password incorreto(s)", ""));
+		}
+	}
+	
+	private void redirecionar(String pagina) throws IOException {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(pagina);
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
